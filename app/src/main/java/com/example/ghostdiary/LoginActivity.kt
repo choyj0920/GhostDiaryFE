8def1c0fe33e335707543fde567dfc75bfac1b0f
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2022-07-13 18:04:21 +0900

    bottom navigationbar

--- app/src/main/java/com/example/ghostdiary/LoginActivity.kt
+++ app/src/main/java/com/example/ghostdiary/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ghostdiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<ImageView>(R.id.btn_login_email).setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
